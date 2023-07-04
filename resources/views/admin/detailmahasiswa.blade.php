a443545fbd1016300e2de64924eab56131176c37
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-07-04 21:18:57 +0700

    feat(admin-mahasiswa): detail mahasiswa layout

--- resources/views/admin/detailmahasiswa.blade.php
+++ resources/views/admin/detailmahasiswa.blade.php
@@ -23,24 +23,105 @@
         <!-- start: page -->
         <div class="row">
             <div class="col-lg-12 mb-3">
+                <section class="card">
+                    <header class="card-header">
+                        <h2 class="card-title">Informasi</h2>
+                        <a href="{{route('adm.mahasiswa')}}"><button class="btn btn-sm btn-danger float-end">KEMBALI</button></a>
+                    </header>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row" style="width: 20%">Nama Mahasiswa</th>
+                                        <td style="width: 5%">:</td>
+                                        <td>{{$dataMahasiswa->name ?? "-"}}</td>
 
-                    <section class="card">
-                        <header class="card-header">
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" style="width: 20%">NPM</th>
+                                        <td style="width: 5%">:</td>
+                                        <td>{{$dataMahasiswa->mahasiswa->nim ?? "-"}}</td>
 
-                            <h2 class="card-title">FORM TAMBAH MAHASISWA</h2>
-                        </header>
-                        <div class="card-body">
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" style="width: 20%">Email</th>
+                                        <td style="width: 5%">:</td>
+                                        <td>{{$dataMahasiswa->email ?? "-"}}</td>
 
-                            <div class="row">
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-lg-6">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row" style="width: 20%">Phone</th>
+                                        <td style="width: 5%">:</td>
+                                        <td>{{$dataMahasiswa->mahasiswa->phone ?? "-"}}</td>
 
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" style="width: 20%">Alamat</th>
+                                        <td style="width: 5%">:</td>
+                                        <td>{{$dataMahasiswa->mahasiswa->alamat ?? "-"}}</td>
+
+                                    </tr>
+
+                                    </tbody>
+                                </table>
                             </div>
 
                         </div>
-
-                    </section>
+                    </div>
+                </section>
             </div>
         </div>
         <!-- end: page -->
+        <div class="row">
+            <div class="col-lg-12 mb-3">
+
+                <section class="card">
+                    <header class="card-header">
+                        <h2 class="card-title">SKS</h2>
+                    </header>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>NO</th>
+                                        <th>MATA KULIAH</th>
+                                        <th>SKS</th>
+                                        <th>UTS</th>
+                                        <th>UAS</th>
+                                        <th>PRESENSI</th>
+                                        <th>TOTAL</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+        </div>
     </section>
     @push('customJS')
 
