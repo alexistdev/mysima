8228c93ba233826770964e5853144b1037a04eae
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-07-04 08:31:28 +0700

    feat(admin-dashboard): add latest mahasiswa table

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Traits\AdminTrait;
 use App\Models\Siswa;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,10 +14,15 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $user= User::with('mahasiswa')->where('role_id',"3")
+            ->orderBy('id','DESC')
+            ->take(5)
+            ->get();
         return view('admin.dashboard', array(
             'judul' => "Dashboard Administrator | MySima",
             'menuUtama' => 'dashboard',
             'menuKedua' => 'dashboard',
+            'dataMahasiswa' => $user,
 
         ));
     }
